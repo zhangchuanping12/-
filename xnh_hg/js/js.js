40a8141864fe453352bd598c1c2f0770482d45b8
zhangchuanping12 <44633076+zhangchuanping12@users.noreply.github.com>
2018-10-31 16:58:59 +0800

    Add files via upload

---
+++ xnh_hg/js/js.js
@@ -0,0 +1,399 @@
+
+$(".img_two").hide()
+$(".img_three").hide()
+$(".img_four").hide()
+$(".img_fir").hide()
+$(".img_six").hide()
+$(".img_seen").hide()
+
+$(".img_ent").hide()
+$(".img_nin").hide()
+$(".img_ten").hide()
+$(".img_yee").hide()
+$(".img_thi").hide();
+$(".img_neen").hide();
+
+$(".ban_two_4").hide()
+$(".ban_two_5").hide()
+$(".ban_two_6").hide();
+
+
+$(".img1").hide()
+$(".img2").hide()
+$(".img3").hide()
+$(".img4").hide();
+
+
+
+$(".ban_seven2").hide()
+$(".ban_seven3").hide()
+$(".ban_seven4").hide()
+$(".ban_seven5").hide();
+//
+$(".ban_three2").hide()
+$(".ban_three3").hide()
+$(".ban_three4").hide()
+$(".ban_three5").hide()
+$(".ban_three6").hide()
+$(".ban_three7").hide()
+$(".ban_three8").hide()
+$(".ban_three9").hide();
+
+
+$(".ban_six4_1").hide()
+$(".ban_six5_2").hide()
+$(".ban_six6_3").hide()
+$(".ban_six10_6").hide()
+$(".ban_six9_5").hide()
+$(".ban_six7_4").hide()
+$(".ban_six8_8").hide();
+
+
+$(".aple").hide();
+$(".aple1").hide();
+$(".aple2").hide();
+$(".aple3").hide();
+$(".aple4").hide();
+$(function(){
+
+$(".banner_one").hover(function(){
+	setInterval(function() {
+			$(".img_two").show().addClass("animated bounceInRight")
+		})
+    	setInterval(function() {
+			$(".img_three").show().addClass("animated bounceInLeft")
+		},100)
+    	setInterval(function() {
+			$(".img_four").show().addClass("animated bounceInLeft")
+		},200)
+    	setInterval(function() {
+			$(".img_fir").show().addClass("animated bounceInLeft")
+		},300)
+    	setInterval(function() {
+			$(".img_six").show().addClass("animated bounceInLeft")
+		},400)
+    	setInterval(function() {
+			$(".img_seen").show().addClass("animated bounceInLeft")
+		},500)
+})
+$(".banner_one").click(function(){
+	setInterval(function() {
+			$(".img_two").show().addClass("animated bounceInRight")
+		})
+    	setInterval(function() {
+			$(".img_three").show().addClass("animated bounceInLeft")
+		},100)
+    	setInterval(function() {
+			$(".img_four").show().addClass("animated bounceInLeft")
+		},200)
+    	setInterval(function() {
+			$(".img_fir").show().addClass("animated bounceInLeft")
+		},300)
+    	setInterval(function() {
+			$(".img_six").show().addClass("animated bounceInLeft")
+		},400)
+    	setInterval(function() {
+			$(".img_seen").show().addClass("animated bounceInLeft")
+		},500)
+})
+$(".banner_one").hover(function(){
+	setInterval(function() {
+			$(".img_ent").show().addClass("animated bounceInRight")
+		})
+    	setInterval(function() {
+			$(".img_nin").show().addClass("animated bounceInRight")
+		},100)
+    	setInterval(function() {
+			$(".img_ten").show().addClass("animated bounceInRight")
+		},200)
+    	setInterval(function() {
+			$(".img_yee").show().addClass("animated bounceInRight")
+		},300)
+    	setInterval(function() {
+			$(".img_thi").show().addClass("animated bounceInRight")
+		},400)
+    	setInterval(function() {
+			$(".img_neen").show().addClass("animated bounceInRight")
+		},500)
+})
+$(".banner_one").click(function(){
+	setInterval(function() {
+			$(".img_ent").show().addClass("animated bounceInRight")
+		})
+    	setInterval(function() {
+			$(".img_nin").show().addClass("animated bounceInRight")
+		},100)
+    	setInterval(function() {
+			$(".img_ten").show().addClass("animated bounceInRight")
+		},200)
+    	setInterval(function() {
+			$(".img_yee").show().addClass("animated bounceInRight")
+		},300)
+    	setInterval(function() {
+			$(".img_thi").show().addClass("animated bounceInRight")
+		},400)
+    	setInterval(function() {
+			$(".img_neen").show().addClass("animated bounceInRight")
+		},500)
+})
+$(".ban_two").hover(function(){
+	setInterval(function() {
+			$(".ban_two_4").show().addClass("animated fadeInLeftBig")
+		},150)
+    	setInterval(function() {
+			$(".ban_two_5").show().addClass("animated fadeInRightBig")
+		},200)
+    	setInterval(function() {
+			$(".ban_two_6").show().addClass("animated fadeInRightBig")
+		},300)
+})
+$(".b").click(function(){
+       setInterval(function() {
+			$(".ban_two_4").show().addClass("animated fadeInLeftBig")
+		},150)
+    	setInterval(function() {
+			$(".ban_two_5").show().addClass("animated fadeInRightBig")
+		},200)
+    	setInterval(function() {
+			$(".ban_two_6").show().addClass("animated fadeInRightBig")
+		},300)
+    })
+
+
+
+$(".ban_four").hover(function(){
+//	setInterval(function() {
+//			$(".ban_four5").show().addClass("animated  fadeInUpBig")
+//		},150)
+//  	setInterval(function() {
+//			$(".ban_four6").show().addClass("animated  fadeInUpBig")
+//		},200)
+//  	setInterval(function() {
+//			$(".ban_four9").show().addClass("animated   fadeInUpBig ")
+//		},150)
+//  	setInterval(function() {
+//			$(".ban_four10").show().addClass("animated   fadeInUpBig ")
+//		},200)
+})
+$(".d").click(function(){
+//	setInterval(function() {
+//			$(".ban_four5").show().addClass("animated  fadeInUpBig")
+//		},150)
+//  	setInterval(function() {
+//			$(".ban_four6").show().addClass("animated  fadeInUpBig")
+//		},200)
+//  	setInterval(function() {
+//			$(".ban_four9").show().addClass("animated   fadeInUpBig ")
+//		},150)
+//  	setInterval(function() {
+//			$(".ban_four10").show().addClass("animated   fadeInUpBig ")
+//		},200)
+})
+$(".ban_four").hover(function(){
+	setInterval(function() {
+			$(".img1").show().addClass("animated  fadeInUpBig")
+		},100)
+    	setInterval(function() {
+			$(".img2").show().addClass("animated  fadeInUpBig")
+		},250)
+    	setInterval(function() {
+			$(".img3").show().addClass("animated   fadeInUpBig ")
+		},450)
+    	setInterval(function() {
+			$(".img4").show().addClass("animated   fadeInUpBig ")
+		},650)
+    	setInterval(function() {
+			$(".aple1").show().addClass("animated   fadeInLeftBig ")
+		},500)
+})
+$(".d").click(function(){
+	setInterval(function() {
+			$(".img1").show().addClass("animated  fadeInUpBig")
+		},150)
+    	setInterval(function() {
+			$(".img2").show().addClass("animated  fadeInUpBig")
+		},200)
+    	setInterval(function() {
+			$(".img3").show().addClass("animated   fadeInUpBig ")
+		},150)
+    	setInterval(function() {
+			$(".img4").show().addClass("animated   fadeInUpBig ")
+		},200)
+    	setInterval(function() {
+			$(".aple1").show().addClass("animated   fadeInLeftBig ")
+		},500)
+})
+$(".ban_six").hover(function(){
+	setInterval(function() {
+			$(".ban_six4_1").show().addClass("animated   bounceInLeft")
+		},250)
+    	setInterval(function() {
+			$(".ban_six5_2").show().addClass("animated   bounceInRight")
+		},200)
+    	setInterval(function() {
+			$(".ban_six6_3").show().addClass("animated   bounceInRight")
+		},300)
+    	setInterval(function() {
+			$(".ban_six10_6").show().addClass("animated   bounceInRight")
+		},400)
+    	setInterval(function() {
+			$(".ban_six9_5").show().addClass("animated   bounceInRight")
+		},500)
+    	setInterval(function() {
+			$(".ban_six7_4").show().addClass("animated   bounceInLeft")
+		},550)
+    	setInterval(function() {
+			$(".ban_six8_8").show().addClass("animated   bounceInRight")
+		},500)
+    	setInterval(function() {
+			$(".aple3").show().addClass("animated   bounceInLeft")
+		},300)
+    	setInterval(function() {
+			$(".aple4").show().addClass("animated   bounceInLeft")
+		},500)
+})
+$(".e").click(function(){
+	setInterval(function() {
+			$(".ban_six4_1").show().addClass("animated   bounceInRight")
+		},100)
+    	setInterval(function() {
+			$(".ban_six5_2").show().addClass("animated   bounceInRight")
+		},200)
+    	setInterval(function() {
+			$(".ban_six6_3").show().addClass("animated   bounceInRight")
+		},300)
+    	setInterval(function() {
+			$(".ban_six10_6").show().addClass("animated   bounceInRight")
+		},400)
+    	setInterval(function() {
+			$(".ban_six9_5").show().addClass("animated   bounceInRight")
+		},500)
+    	setInterval(function() {
+			$(".ban_six7_4").show().addClass("animated   bounceInRight")
+		},500)
+    	setInterval(function() {
+			$(".ban_six8_8").show().addClass("animated   bounceInRight")
+		},500)
+    	setInterval(function() {
+			$(".aple3").show().addClass("animated   bounceInLeft")
+		},300)
+    	setInterval(function() {
+			$(".aple4").show().addClass("animated   bounceInLeft")
+		},500)
+})
+$(".ban_seven").hover(function(){
+	setInterval(function() {
+			$(".ban_seven2").show().addClass("animated   bounceInLeft")
+		},150)
+    	setInterval(function() {
+			$(".ban_seven3").show().addClass("animated   bounceInLeft")
+		},300)
+    	setInterval(function() {
+			$(".ban_seven4").show().addClass("animated   bounceInLeft")
+		},400)
+    	setInterval(function() {
+			$(".ban_seven5").show().addClass("animated   bounceInLeft")
+		},500)
+    	setInterval(function() {
+			$(".aple2").show().addClass("animated   bounceInLeft")
+		},500)
+})
+$(".f").click(function(){
+	setInterval(function() {
+			$(".ban_seven2").show().addClass("animated   bounceInLeft")
+		},150)
+    	setInterval(function() {
+			$(".ban_seven3").show().addClass("animated   bounceInLeft")
+		},300)
+    	setInterval(function() {
+			$(".ban_seven4").show().addClass("animated   bounceInLeft")
+		},400)
+    	setInterval(function() {
+			$(".ban_seven5").show().addClass("animated   bounceInLeft")
+		},500)
+    	setInterval(function() {
+			$(".aple2").show().addClass("animated   bounceInLeft")
+		},500)
+})
+//
+$(".ban_three").hover(function(){
+	setInterval(function() {
+			$(".ban_three5").show().addClass("animated   fadeInLeftBig ")
+		},100)
+    	setInterval(function() {
+			$(".ban_three2").show().addClass("animated   fadeInDownBig ")
+		},500)
+    	setInterval(function() {
+			$(".ban_three3").show().addClass("animated   fadeInDownBig ")
+		},600)
+    	setInterval(function() {
+			$(".ban_three4").show().addClass("animated   fadeInDownBig ")
+		},700)
+    	setInterval(function() {
+			$(".ban_three6").show().addClass("animated   fadeInRightBig ")
+		},800)
+    	setInterval(function() {
+			$(".ban_three9").show().addClass("animated   fadeInUpBig")
+		},200)
+    	setInterval(function() {
+			$(".ban_three8").show().addClass("animated   fadeInUpBig ")
+		},400)
+    	setInterval(function() {
+			$(".ban_three7").show().addClass("animated   fadeInUpBig ")
+		},500)
+    	setInterval(function() {
+			$(".aple").show().addClass("animated   fadeInLeftBig ")
+		},500)
+})
+$(".c").click(function(){
+	setInterval(function() {
+			$(".ban_three5").show().addClass("animated   fadeInLeftBig ")
+		},100)
+    	setInterval(function() {
+			$(".ban_three2").show().addClass("animated   fadeInDownBig ")
+		},500)
+    	setInterval(function() {
+			$(".ban_three3").show().addClass("animated   fadeInDownBig ")
+		},600)
+    	setInterval(function() {
+			$(".ban_three4").show().addClass("animated   fadeInDownBig ")
+		},700)
+    	setInterval(function() {
+			$(".ban_three6").show().addClass("animated   fadeInRightBig ")
+		},800)
+    	setInterval(function() {
+			$(".ban_three9").show().addClass("animated   fadeInUpBig")
+		},700)
+    	setInterval(function() {
+			$(".ban_three8").show().addClass("animated   fadeInUpBig ")
+		},400)
+    	setInterval(function() {
+			$(".ban_three7").show().addClass("animated   fadeInUpBig ")
+		},200)
+    	setInterval(function() {
+			$(".aple").show().addClass("animated   fadeInLeftBig ")
+		},500)
+})
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
